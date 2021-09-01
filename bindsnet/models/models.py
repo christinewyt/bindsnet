e9d1bfb479e5c708543618c845f1415664ed1110
christinewyt <85403478+christinewyt@users.noreply.github.com>
2021-09-01 08:49:01 -0400

    Update models.py

--- bindsnet/models/models.py
+++ bindsnet/models/models.py
@@ -656,7 +656,7 @@ class DiehlAndCook2015_withDopamin(Network):
             nu=nu,
             reduction=reduction,
             wmin=0.0,
-            wmax=0.5,
+            wmax=0.3,
             **kwargs
         )
         w = self.exc * torch.diag(torch.ones(self.n_neurons))
