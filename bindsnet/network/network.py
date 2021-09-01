69238759ecaeeba4b7063ab1cbc21e417cfa847a
christinewyt <85403478+christinewyt@users.noreply.github.com>
2021-09-01 08:43:40 -0400

    Update network.py

--- bindsnet/network/network.py
+++ bindsnet/network/network.py
@@ -692,7 +692,7 @@ class Network(torch.nn.Module):
                 break
 
         # Mark the number of spikes excitatory neurons have emitted
-        n_neuron = self.connections[('X', 'Ae')].shape[1]
+        n_neuron = self.connections[('X', 'Ae')].w.shape[1]
         Flag_spike = torch.tensor([n_neuron, n_dopamin_spike], device = self._get_inputs(layers=['Ae'])['Ae'].device)
         print(Flag_spike.shape)
         # Mark whether dopamin signal has been applied
